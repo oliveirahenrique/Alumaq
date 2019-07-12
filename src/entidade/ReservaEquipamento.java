e3b5002ae5f161373bcdb01079bedbd776a30cf2
Henrique Alves de Oliveira <a14036@bcc.unifal-mg.edu.br>
2019-07-12 16:23:44 -0300

    transformando todos em entidade

--- src/entidade/ReservaEquipamento.java
+++ src/entidade/ReservaEquipamento.java
@@ -1,12 +1,29 @@
 package entidade;
 
-public class ReservaEquipamento {
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity
+public class ReservaEquipamento implements Serializable{
+
     private Cliente cliente;
     private Equipamento equipamento;
+    @Id
+    private Long idReserva;
     
     public ReservaEquipamento (Cliente cliente, Equipamento equipamento) {
         this.cliente = cliente;
         this.equipamento = equipamento;
+    }
+
+    public Long getIdReserva() {
+        return idReserva;
+    }
+
+    public void setIdReserva(Long idReserva) {
+        this.idReserva = idReserva;
     }
 }
 

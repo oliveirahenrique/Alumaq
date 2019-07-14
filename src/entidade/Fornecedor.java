3f42cd2cb00095e9529e871f92f3bb0c96a06005
machadorafa <rafafsmachado@gmail.com>
2019-07-14 18:17:53 -0300

    enviar email ao fornecedor da compra de equipamentos

--- src/entidade/Fornecedor.java
+++ src/entidade/Fornecedor.java
@@ -30,5 +30,22 @@ public class Fornecedor implements Serializable{
     public void setID(Integer idFornecedor) {
         this.idFornecedor = idFornecedor;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
     
 }

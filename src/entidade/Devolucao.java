fe02813388fa9f8bd32cddc326eaab49fab4b41c
Vitor Muniz <33725123+vitormuuniz@users.noreply.github.com>
2019-07-14 20:03:42 -0300

    Implementada Devolucao.java

--- src/entidade/Devolucao.java
+++ src/entidade/Devolucao.java
@@ -1,12 +1,28 @@
 package entidade;
 
+import java.util.Date;
+import persistencia.DAO;
+
 public class Devolucao {
+
     private Contrato contrato;
     private Pagamento pagamento;
     private Funcionario f;
+    private double valorTotal;
+    public Devolucao() {
+    }
     
-    public Devolucao(Contrato c) {
-      contrato = c;
-      contrato.setFase(Fase.FINALIZADO);
+    public double devolverLocacao(Contrato c, Date dataDevolucao){
+        contrato = c;
+        if (contrato.getTipo().equals(Tipo.LOCACAO)) {
+            if (contrato.getFase().equals(Fase.FASE2)) {
+                this.valorTotal = c.getValorp1() + c.getValorp2() + c.getMulta(c, dataDevolucao);
+                contrato.setFase(Fase.FINALIZADO);
+                DAO dao = new DAO();
+                dao.atualizar(c);
+                dao.fechar();
+            }
+        }
+        return this.valorTotal;
     }
 }

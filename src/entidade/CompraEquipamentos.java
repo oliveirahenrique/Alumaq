2e0c5f30e440d23fd4b36b87c720ba7d62942687
Rafael Machado <33268419+machadorafa@users.noreply.github.com>
2019-07-14 16:26:17 -0300

    implementação de Compra Equipamentos

--- src/entidade/CompraEquipamentos.java
+++ src/entidade/CompraEquipamentos.java
@@ -1,7 +1,29 @@
 package entidade;
+import persistencia.DAO;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class CompraEquipamentos {
+    ArrayList<Equipamento> equipamentos;
+
+    public CompraEquipamentos(ArrayList<Equipamento> equipamentos) {
+        this.equipamentos = equipamentos;
+    }
     
+    public String comprar() {
+        Double valor = 0.0;
+        String r = " ";
+        DAO dao = new DAO();
+        for (Equipamento e : equipamentos) {
+            valor += e.getValor();
+            e.qtd = e.qtd + 1;
+            e.setDisponivel(true);
+
+            dao.atualizar(e);
+        }
+        dao.fechar();
+        r = "Compra realizada com sucesso! Valor: R$"+valor;
+        
+        return r;
+    }
 }

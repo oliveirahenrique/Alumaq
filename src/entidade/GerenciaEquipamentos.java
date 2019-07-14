02d8f9abe071dbc2ecbee941d7872302af23f7cf
Vitor Muniz <33725123+vitormuuniz@users.noreply.github.com>
2019-07-14 15:00:09 -0300

    Implementando GerenciaEquipamentos.java

--- src/entidade/GerenciaEquipamentos.java
+++ src/entidade/GerenciaEquipamentos.java
@@ -1,29 +1,42 @@
 package entidade;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.List;
+import persistencia.DAO;
 
 public class GerenciaEquipamentos {
+
     private ControladorEstoque controle = new ControladorEstoque();
     private Equipamento equipamento;
-    
-    public boolean insere (Equipamento equipamento, Setor setor) {
-        boolean equipamentoInserido= false;
+
+    public boolean insere(Equipamento equipamento, Setor setor) {
+        boolean equipamentoInserido = false;
         //Query para inserir Equipamento no banco de dados
-        
-         return equipamentoInserido;
+
+        return equipamentoInserido;
     }
-    
-    public void exclui (Equipamento equipamento, Setor setor) {
-        //Query para excluir equipamento do banco de dados
+
+    public void exclui(Equipamento equipamento, Setor setor) {
+        if (equipamento.getSetor().equals(setor)) {
+            DAO dao = new DAO();
+            dao.remover(equipamento);
+            dao.fechar();
+        }
     }
-    
-    public void editar (int id, Setor setor) {
-        //Query para editar um equipamento no banco de dados
+
+    public void editar(Equipamento equipamento, Setor setor) {
+        equipamento.setSetor(setor);
+        DAO dao = new DAO();
+        dao.atualizar(equipamento);
+        dao.fechar();
     }
-    
-    public List<Equipamento> consultar (Setor setor) {
+
+    public void consultarEquipamentos() {
         //Query para consultar equipamentos de um setor
-        return null;
+        DAO dao = new DAO();
+        dao.getListEquipamentos();
+        dao.fechar();
     }
 }
-

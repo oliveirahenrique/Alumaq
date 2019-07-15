59bc063ee9b54aa904c850ad606fad48731471b0
Vitor Muniz <33725123+vitormuuniz@users.noreply.github.com>
2019-07-15 07:43:01 -0300

    Implementada Devolucao.java

--- src/entidade/Devolucao.java
+++ src/entidade/Devolucao.java
@@ -5,19 +5,18 @@ import persistencia.DAO;
 
 public class Devolucao {
 
-    private Contrato contrato;
-    private Pagamento pagamento;
-    private Funcionario f;
+    //private Pagamento pagamento;
+    //private Funcionario f;
     private double valorTotal;
+    
     public Devolucao() {
     }
     
     public double devolverLocacao(Contrato c, Date dataDevolucao){
-        contrato = c;
-        if (contrato.getTipo().equals(Tipo.LOCACAO)) {
-            if (contrato.getFase().equals(Fase.FASE2)) {
+        if (c.getTipo().equals(Tipo.LOCACAO)) {
+            if (c.getFase().equals(Fase.FASE2)) {
                 this.valorTotal = c.getValorp1() + c.getValorp2() + c.getMulta(c, dataDevolucao);
-                contrato.setFase(Fase.FINALIZADO);
+                c.setFase(Fase.FINALIZADO);
                 DAO dao = new DAO();
                 dao.atualizar(c);
                 dao.fechar();

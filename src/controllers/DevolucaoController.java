c9f3f07b965a6bf96cf2412e88a04f45895bd218
machadorafa <rafafsmachado@gmail.com>
2019-07-18 04:11:43 -0300

    devolução tela e controller

--- src/controllers/DevolucaoController.java
+++ src/controllers/DevolucaoController.java
@@ -5,9 +5,14 @@
  */
 package controllers;
 
+import entidade.Equipamento;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 /**
  * FXML Controller class
@@ -15,6 +20,23 @@ import javafx.fxml.Initializable;
  * @author natalia
  */
 public class DevolucaoController implements Initializable, Controller {
+    
+    @FXML
+    private TableView<Equipamento> tv_contratos;
+
+    @FXML
+    private TableColumn<Equipamento, Integer> tc_id;
+
+    @FXML
+    private TableColumn<Equipamento, String> tc_cliente;
+
+    @FXML
+    private TableColumn<Equipamento, Double> tc_funcionario;
+    
+    @FXML
+    void clica_selecionar(ActionEvent event) {
+
+    }
 
     /**
      * Initializes the controller class.

888328a8310a7a1bcb579b5e3cd650ad7a8573ac
machadorafa <rafafsmachado@gmail.com>
2019-07-18 02:30:48 -0300

    atualizada tela de cadastro de funcionario e bug no controller

--- src/controllers/CadastrarFuncionarioController.java
+++ src/controllers/CadastrarFuncionarioController.java
@@ -24,6 +24,12 @@ import telasFXML.Main;
 public class CadastrarFuncionarioController implements Initializable, Controller {
 
     @FXML
+    private Button btn_salvar;
+
+    @FXML
+    private Button btn_cancelar;
+
+    @FXML
     private TextField tf_nome;
 
     @FXML
@@ -33,16 +39,10 @@ public class CadastrarFuncionarioController implements Initializable, Controller
     private TextField tf_salario;
 
     @FXML
-    private Button btn_salvar;
-
-    @FXML
-    private Button btn_cancelar;
-
-    @FXML
     private TextField tf_tel1;
 
     @FXML
-    private TextField tf_tel2;
+    private TextField tf_complemento;
 
     @FXML
     private TextField tf_rua;
@@ -58,42 +58,42 @@ public class CadastrarFuncionarioController implements Initializable, Controller
 
     @FXML
     private TextField tf_estado;
-    
-    @FXML
-    private TextField tf_complemento;
-    
+
     @FXML
     private TextField tf_cpf;
-    
+
+    @FXML
+    private TextField tf_tel2;
+
     @FXML
     private TextField tf_login;
-    
+
     @FXML
     private TextField tf_senha;
-    
+
     @FXML
     private TextField tf_cargoId;
 
     @FXML
-    void clica_salvar(ActionEvent event) throws ParseException {        
+    void clica_salvar(ActionEvent event) throws ParseException {
         // nome do funcionario
         String nome = this.tf_nome.getText();
-        
+
         // data de nascimento do funcionario
         Date dataNascimento = new SimpleDateFormat("dd/MM/yyyy").parse(this.tf_dataNasc.getText());
-        
+
         // salario do funcionario
         Double salario = Double.parseDouble(this.tf_salario.getText());
-        
+
         // telefone1 do funcionario
         String telefone1 = this.tf_tel1.getText();
-        
+
         // telefone2 do funcionario
         String telefone2 = this.tf_tel2.getText();
-        
+
         // cpf do funcionario
         String cpf = this.tf_cpf.getText();
-        
+
         // endereço do funcionario
         String rua = this.tf_rua.getText();
         int numero = Integer.parseInt(this.tf_num.getText());
@@ -108,11 +108,11 @@ public class CadastrarFuncionarioController implements Initializable, Controller
             endereco = new Endereco(rua, bairro, complemento, numero, cidade, estado);
         }
         dao.cadastrar(endereco);
-        
+
         // cadastra novo funcionario
         Funcionario funcionario = new Funcionario(nome, cpf, dataNascimento, endereco, telefone1, telefone2, salario);
         dao.cadastrar(funcionario);
-        
+
         // cadastra novo usuario
         String login = this.tf_login.getText();
         String senha = this.tf_senha.getText();
@@ -132,22 +132,22 @@ public class CadastrarFuncionarioController implements Initializable, Controller
 
     @FXML
     void clica_cancela(ActionEvent event) {
-//        tf_bairro.setText("");
-//        tf_cargoId.setText("");
-//        tf_cidade.setText("");
-//        tf_complemento.setText("");
-//        tf_cpf.setText("");
-//        tf_dataNasc.setText("");
-//        tf_estado.setText("");
-//        tf_login.setText("");
-//        tf_nome.setText("");
-//        tf_num.setText("");
-//        tf_rua.setText("");
-//        tf_salario.setText("");
-//        tf_senha.setText("");
-//        tf_tel1.setText("");
-//        tf_tel2.setText("");
-        
+        this.tf_bairro.setText("");
+        this.tf_cargoId.setText("");
+        this.tf_cidade.setText("");
+        this.tf_complemento.setText("");
+        this.tf_cpf.setText("");
+        this.tf_dataNasc.setText("");
+        this.tf_estado.setText("");
+        this.tf_login.setText("");
+        this.tf_nome.setText("");
+        this.tf_num.setText("");
+        this.tf_rua.setText("");
+        this.tf_salario.setText("");
+        this.tf_senha.setText("");
+        this.tf_tel1.setText("");
+        this.tf_tel2.setText("");
+
         Main.changeScreen("index");
     }
 
